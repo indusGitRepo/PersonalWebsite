39e004cb6432cdc4ad0ced3e7dede739efbe3909
Inderjeet <94882715+Inderjeet3@users.noreply.github.com>
2024-08-21 18:51:05 -0400

    projectspage

--- skills.js
+++ skills.js
@@ -18,3 +18,21 @@ function toggleClass() {
     document.querySelector('.navbar_menu').classList.toggle('active');
     document.querySelector('.imgs').classList.toggle('active');
 }
+
+// Select the images by their classes
+const image1 = document.querySelector('.Amino');
+const image2 = document.querySelector('.Excel');
+const image3 = document.querySelector('.AminoFlock');
+
+// Add event listeners to each image
+image1.addEventListener('click', () => {
+    image1.classList.toggle('clicked');
+});
+
+image2.addEventListener('click', () => {
+    image2.classList.toggle('clicked');
+});
+
+image3.addEventListener('click', () => {
+    image3.classList.toggle('clicked');
+});
